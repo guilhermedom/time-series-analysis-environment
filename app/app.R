35be286985ed37418eca4453a00e251f2d74f3ae
guilhermedom <guilhermedfs@gmail.com>
2022-08-22 21:00:53 -0300

    style: reformat script for better readability
    Wrap lines around 80 char length. Adjust function argument formatting. Rename variables to make them more intuitive.

--- app/app.R
+++ app/app.R
@@ -16,20 +16,29 @@ ui = fluidPage(
     
     fluidRow(
         column(4, align = "center",
-               fileInput("inputFileID", "Choose file to load time series:",
-                         multiple = F, accept = c(".csv")),
+               fileInput("fileInputID",
+                         "Choose file to load time series:",
+                         accept = c("text/csv",
+                                    "text/comma-separated-values",
+                                    "text/plain",
+                                    ".csv")),
                helpText("Note: file must contain only one column, without header.
                         Time series frequency must be in months.")
         ),
         column(4, align = "center",
-               dateRangeInput("dateRangeInputID", label = "Time series date range",
-                              format = "mm/yyyy", start = "2000/01/01", end = "2013/12/31",
-                              startview = "year", separator = " to "),
-               helpText("Note: you may select any day when defining month and year.")
+               dateRangeInput("dateRangeInputID",
+                              label = "Time series date range",
+                              format = "mm/yyyy",
+                              start = "2000/01/01",
+                              end = "2013/12/31",
+                              startview = "year",
+                              separator = " to "),
+               helpText("Note: you may select any day when defining month
+                        and year.")
         ),
         column(4, align = "center",
-               numericInput("forecastPeriodID", "How many months do you want to forecast?",
-                            value = 12, min = 1, max = 48),
+               numericInput("forecastPeriodID", "How many months do you want to
+                            forecast?", value = 12, min = 1, max = 48),
                actionButton("forecastButtonID", "Forecast!")
         )
     ),
@@ -61,49 +70,61 @@ ui = fluidPage(
 server = function(input, output) {
     observeEvent(input$forecastButtonID, {
         validate(
-            need(input$inputFileID, "Please provide a file having a time series to analyze.")
+            need(input$fileInputID, "Please provide a file having a time series
+                 to analyze.")
         )
         
-        inputFile = input$inputFileID
-        data = read.csv(inputFile$datapath, header = F)
+        data = read.csv(input$fileInputID$datapath, header = F)
         
         startYear = as.integer(substr(input$dateRangeInputID[1], 1, 4))
         startMonth = as.integer(substr(input$dateRangeInputID[1], 6, 7))
         endYear = as.integer(substr(input$dateRangeInputID[2], 1, 4))
         endMonth = as.integer(substr(input$dateRangeInputID[2], 6, 7))
         
-        data = ts(data, start = c(startYear, startMonth), end = c(endYear, endMonth),
-                  frequency = 12)
+        timeSeries = ts(data, start = c(startYear, startMonth),
+                        end = c(endYear, endMonth), frequency = 12)
         
         output$histogramPlotID = renderPlot({
-            hist(data, xlab = "Data", main = "Data Histogram")
+            hist(timeSeries, xlab = "Data", main = "Data Histogram")
         })
         output$boxplotPlotID = renderPlot({
-            boxplot(data, main = "Data Boxplot")
+            boxplot(timeSeries, main = "Data Boxplot")
         })
         output$timeseriesPlotID = renderPlot({
-            autoplot(data, ylab = "Data", main = "Original Time Series")
+            autoplot(timeSeries, ylab = "Data", main = "Original Time Series")
         })
         
-        decomposedSeries = decompose(data)
+        decomposedSeries = decompose(timeSeries)
         output$decompositionPlotID = renderPlot({
             autoplot(decomposedSeries, main = "Decomposed Time Series")
         })
         
-        arimaModel = auto.arima(data)
+        arimaModel = auto.arima(timeSeries)
         forecastPeriod = input$forecastPeriodID
         prediction = forecast(arimaModel, h = forecastPeriod)
         
-        output$lowerBoundTableID = renderTable({prediction$lower})
-        output$meanTableID = renderTable({prediction$mean})
-        output$upperBoundTableID = renderTable({prediction$upper})
+        output$lowerBoundTitleID = renderText({
+            "Lower Bound"
+        })
+        output$meanTitleID = renderText({
+            "Mean"
+        })
+        output$upperBoundTitleID = renderText({
+            "Upper Bound"
+        })
         
-        output$lowerBoundTitleID = renderText({"Lower Bound"})
-        output$meanTitleID = renderText({"Mean"})
-        output$upperBoundTitleID = renderText({"Upper Bound"})
+        output$lowerBoundTableID = renderTable({
+            prediction$lower
+        })
+        output$meanTableID = renderTable({
+            prediction$mean
+        })
+        output$upperBoundTableID = renderTable({
+            prediction$upper
+        })
         
         output$forecastPlotID = renderPlot({
-            autoplot(prediction, ylab = "Data", main = "Forecasted Time Series")
+            autoplot(prediction, main = "Forecasted Time Series", ylab = "Data")
         })
     })
 }
